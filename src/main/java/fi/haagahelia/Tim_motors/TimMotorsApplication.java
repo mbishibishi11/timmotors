cba9fdfaa800afefeed3de99e4e75e42c6be6bc3
Leon Mbishibishi <mbishibishilyon11@gmail.com>
2024-11-02 09:26:45 +0200

    Initial commit

---
+++ src/main/java/fi/haagahelia/Tim_motors/TimMotorsApplication.java
@@ -0,0 +1,13 @@
+package fi.haagahelia.Tim_motors;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class TimMotorsApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(TimMotorsApplication.class, args);
+	}
+
+}
---
+++ src/test/java/fi/haagahelia/Tim_motors/TimMotorsApplicationTests.java
@@ -0,0 +1,13 @@
+package fi.haagahelia.Tim_motors;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class TimMotorsApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
