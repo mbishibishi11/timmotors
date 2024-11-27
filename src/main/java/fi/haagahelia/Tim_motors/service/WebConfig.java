3ce8bdd631bf59142c9302331c81f94633af26c2
Leon Mbishibishi <mbishibishilyon11@gmail.com>
2024-11-28 01:57:00 +0200

    Fixed file upload path

--- src/main/java/fi/haagahelia/Tim_motors/service/WebConfig.java
+++ src/main/java/fi/haagahelia/Tim_motors/service/WebConfig.java
@@ -10,6 +10,6 @@ public class WebConfig implements WebMvcConfigurer {
     @Override
     public void addResourceHandlers(ResourceHandlerRegistry registry) {
         registry.addResourceHandler("/uploads/**")
-                .addResourceLocations("classpath:/static/uploads/");
+                .addResourceLocations("file:uploads/");
     }
 }
